2d9fffe5b7e016d351f754c3fcade9a8ed2f150a
H23sk1spkt <23115141122103@sv.ute.udn.vn>
2025-09-27 18:47:44 +0700

    finish

--- app/src/main/java/com/example/btcn_tuan2/QAPage.java
+++ app/src/main/java/com/example/btcn_tuan2/QAPage.java
@@ -1,24 +1,49 @@
 package com.example.btcn_tuan2;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class QAPage extends AppCompatActivity {
+
+    Button btnA, btnB, btnC, btnD;
+    boolean isAnswered = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_qapage);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        btnA = findViewById(R.id.btnA);
+        btnB = findViewById(R.id.btnB);
+        btnC = findViewById(R.id.btnC);
+        btnD = findViewById(R.id.btnD);
+
+        // Câu trả lời đúng là D
+        btnA.setOnClickListener(v -> handleAnswer(btnA, false));
+        btnB.setOnClickListener(v -> handleAnswer(btnB, false));
+        btnC.setOnClickListener(v -> handleAnswer(btnC, false));
+        btnD.setOnClickListener(v -> handleAnswer(btnD, true));
+    }
+
+    private void handleAnswer(Button selectedButton, boolean isCorrect) {// Đã chọn thì không xử lý nữa
+
+        if (isCorrect) {
+            selectedButton.setBackgroundColor(Color.GREEN);
+        } else {
+            selectedButton.setBackgroundColor(Color.RED);
+        }
+
+        // Ẩn màu đỏ nếu chọn đúng (chỉ hiện màu xanh)
+        if (isCorrect) {
+            btnA.setBackgroundColor(Color.TRANSPARENT);
+            btnB.setBackgroundColor(Color.TRANSPARENT);
+            btnC.setBackgroundColor(Color.TRANSPARENT);
+            btnD.setBackgroundColor(Color.TRANSPARENT);
+            selectedButton.setBackgroundColor(Color.GREEN);
+        }
+
     }
 }
