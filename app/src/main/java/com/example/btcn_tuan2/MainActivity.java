d5bb087f7e32cc36d5b5cf32b2b213c8ec07c452
H23sk1spkt <23115141122103@sv.ute.udn.vn>
2025-09-27 18:24:46 +0700

    create method on click

--- app/src/main/java/com/example/btcn_tuan2/MainActivity.java
+++ app/src/main/java/com/example/btcn_tuan2/MainActivity.java
@@ -1,24 +1,31 @@
 package com.example.btcn_tuan2;
 
+// Trong MainActivity.java
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        setContentView(R.layout.activity_main); // layout chính
+
+        btnRegister = findViewById(R.id.btnRegister);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Chuyển sang RegisterSuccessActivity
+                Intent intent = new Intent(MainActivity.this, QAPage.class);
+                startActivity(intent);
+            }
         });
     }
 }
